6244c1c3d55de7ac194da53c88ea427830cfabc6
Kailash Sutahr <kailashsuthar4488@gmail.com>
2026-03-05 11:41:17 +0530

    complete

--- JWT/src/routes/user.route.js
+++ JWT/src/routes/user.route.js
@@ -34,26 +34,28 @@ userAuthentication.post("/register",async(req,res)=>{
        } 
     }catch(error){
        res.status(400).json({
-        message : "Something Went Wrong"
+        message : "Something Went Wrong",
+        error
        })
     }
 })
 
-userrAuthentication.get("/users",async(req,res)=>{
+userAuthentication.get("/users",async(req,res)=>{
     try{
         const count = await userModel.find();
-        if(count===0){
-         return res.staus(300).json({
+        if(count.length === 0){
+         return res.status(300).json({
                 message : "User Not Added"
             })
         }
-        res.status(201).json({
+        res.status(200).json({
             message : "User Fetch Successfully",
             count
         })
     }catch(error){
          res.status(400).json({
-            message : "Something Went Wrong"
+            message : "Something Went Wrong",
+            error
          })
     }
 })
@@ -62,9 +64,42 @@ userAuthentication.patch("/users/:id",async(req,res)=>{
     try{
        const id = req.params.id;
        const {name,password,email} = req.body;
-
+       await userModel.findByIdAndUpdate(id,{name,email,password})
+       const isUserExists =await userModel.findOne({email});
+       if(!!isUserExists){
+        res.status(400).json({
+            message : "User Already Exists with this email"
+        })
+       } else {
+       res.status(201).json({
+        message : "User Updated Successfully"
+       })
+    }
     }catch(error){
+       res.status(400).json({
+        message : "Something Went Wrong",
+        error
+       })
+    }
+})
 
+userAuthentication.delete("/users/:id",async(req,res)=>{
+    try{
+      const id = req.params.id;
+      const isUserExits = await userModel.findById({id})
+      if(!isUserExits){
+        return res.status(400).json({
+            message : "User Not Exists With This ID"
+        })
+    }
+    await userModel.findByIdAndDelete(id);
+    res.status(201).json({
+        message : "User Deleted Successfully"
+    })
+    }catch(error){
+        res.status(400).json({
+            message : "Something Went Wrong"
+        })
     }
 })
 
